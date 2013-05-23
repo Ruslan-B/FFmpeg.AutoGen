33a978ee8305338dc91f3473489883e5ca66a24a
Ruslan Balanukhin <ruslan.balanukhin@gmail.com>
2013-05-23 16:54:44 +0200

    updated accordingly v1.2

--- FFmpeg/tools/create-symlinks.cmd
+++ FFmpeg/tools/create-symlinks.cmd
@@ -1,7 +1,7 @@
-mklink avcodec.dll avcodec-55.dll
-mklink avdevice.dll avdevice-55.dll
+mklink avcodec.dll avcodec-54.dll
+mklink avdevice.dll avdevice-54.dll
 mklink avfilter.dll avfilter-3.dll
-mklink avformat.dll avformat-55.dll
+mklink avformat.dll avformat-54.dll
 mklink avutil.dll avutil-52.dll
 mklink postproc.dll postproc-52.dll
 mklink swresample.dll swresample-0.dll
