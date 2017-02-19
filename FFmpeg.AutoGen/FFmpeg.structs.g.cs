1a2622184d9cc0ad2cb82a572672e42eaf335424
Ruslan Balanukhin <ruslan.balanukhin@gmail.com>
2017-02-19 15:53:42 +0100

    support for bit fields and fix for AVIndexEntry

--- FFmpeg.AutoGen/FFmpeg.structs.g.cs
+++ FFmpeg.AutoGen/FFmpeg.structs.g.cs
@@ -1053,6 +1053,8 @@ namespace FFmpeg.AutoGen
         public long @pos;
         /// <summary>Timestamp in AVStream.time_base units, preferably the time from which on correctly decoded frames are available when seeking to this entry. That means preferable PTS on keyframe based formats. But demuxers can choose to store a different timestamp, if it is more convenient for the implementation or nothing better is known</summary>
         public long @timestamp;
+        /// <summary>Flag is used to indicate which frame should be discarded after decoding.</summary>
+        public int @flags2_size30;
         /// <summary>Minimum distance between this and the previous keyframe, used to avoid unneeded searching.</summary>
         public int @min_distance;
     }
