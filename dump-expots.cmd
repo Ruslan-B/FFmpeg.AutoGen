6443de2126175eed14c78e6f371c26801dabafaa
Ruslan Balanukhin <ruslan.balanukhin@gmail.com>
2017-09-14 23:27:25 +0200

    Adapt generator to work VC without additional includes.

--- dump-expots.cmd
+++ dump-expots.cmd
@@ -1,4 +1,4 @@
-set dumpbin="%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Community\SDK\ScopeCppSDK\VC\bin\dumpbin.exe" 
+set dumpbin="%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\bin\HostX64\x64\dumpbin.exe" 
 del .\ffmpeg\exports.txt
 cd .\ffmpeg\bin\x64\
 %dumpbin% /EXPORTS avcodec-57.dll avdevice-57.dll avfilter-6.dll avformat-57.dll avutil-55.dll postproc-54.dll swresample-2.dll swscale-4.dll >> ..\..\exports.txt
