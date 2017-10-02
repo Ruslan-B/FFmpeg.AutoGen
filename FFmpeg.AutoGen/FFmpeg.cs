8d2afccd9bce4000de901136dfcfef2f2307a3d8
Ruslan Balanukhin <ruslan.balanukhin@gmail.com>
2017-10-02 10:36:15 +0200

    Improve macro generation AV_VERSION and AV_VERSION_INT support.

--- FFmpeg.AutoGen/FFmpeg.cs
+++ FFmpeg.AutoGen/FFmpeg.cs
@@ -12,5 +12,14 @@ namespace FFmpeg.AutoGen
 
         public static ulong UINT64_C<T>(T a)
             => Convert.ToUInt64(a);
+
+        public static int AV_VERSION_INT<T1, T2, T3>(T1 a, T2 b, T3 c) =>
+            Convert.ToInt32(a) << 16 | Convert.ToInt32(b) << 8 | Convert.ToInt32(c);
+
+        public static string AV_VERSION_DOT<T1, T2, T3>(T1 a, T2 b, T3 c) 
+            => $"{a}.{b}.{c}";
+
+        public static string AV_VERSION<T1, T2, T3>(T1 a, T2 b, T3 c) 
+            => AV_VERSION_DOT(a, b, c);
     }
 }
