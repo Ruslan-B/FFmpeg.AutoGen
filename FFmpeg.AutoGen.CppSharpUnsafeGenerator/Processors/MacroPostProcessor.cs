964bacf891be0aad3fc91d8f5be3475151c94db8
Ruslan Balanukhin <ruslan.balanukhin@gmail.com>
2017-10-04 11:25:41 +0200

    Update ClangParser and regenerate bindings. Closes #70.

--- FFmpeg.AutoGen.CppSharpUnsafeGenerator/Processors/MacroPostProcessor.cs
+++ FFmpeg.AutoGen.CppSharpUnsafeGenerator/Processors/MacroPostProcessor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using FFmpeg.AutoGen.CppSharpUnsafeGenerator.Definitions;
 using RCore.ClangMacroParser;
