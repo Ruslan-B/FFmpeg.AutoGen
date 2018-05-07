a19e3589c12f302aaaa7782916b02ee8e4485b13
Ruslan Balanukhin <ruslan.balanukhin@gmail.com>
2018-05-07 13:24:38 +0200

    Improve native library loading. Add customizable GetPlatformId, GetNativeLibraryName. Closes #89.

--- FFmpeg.AutoGen/Native/LibraryLoader.cs
+++ FFmpeg.AutoGen/Native/LibraryLoader.cs
@@ -1,11 +1,53 @@
-﻿using System;
+﻿// Copyright 2018 Baker Hughes. All rights reserved. Company proprietary and confidential.
+
+using System;
 using System.IO;
 using System.Runtime.InteropServices;
 
 namespace FFmpeg.AutoGen.Native
 {
+    public delegate PlatformID GetPlatformId();
+
+    public delegate string GetNativeLibraryName(string libraryName, int version);
+
     public static class LibraryLoader
     {
+        static LibraryLoader()
+        {
+            GetPlatformId = () =>
+            {
+#if NET45
+                return Environment.OSVersion.Platform;
+#else
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return PlatformID.Win32NT;
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return PlatformID.Unix;
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) return PlatformID.MacOSX;
+                throw new PlatformNotSupportedException();
+#endif
+            };
+
+            GetNativeLibraryName = (libraryName, version) =>
+            {
+                switch (GetPlatformId())
+                {
+                    case PlatformID.MacOSX:
+                        return $"lib{libraryName}.{version}.dylib";
+                    case PlatformID.Unix:
+                        return $"lib{libraryName}.so.{version}";
+                    case PlatformID.Win32NT:
+                    case PlatformID.Win32S:
+                    case PlatformID.Win32Windows:
+                        return $"{libraryName}-{version}.dll";
+                    default:
+                        throw new PlatformNotSupportedException();
+                }
+            };
+        }
+
+        public static GetPlatformId GetPlatformId;
+
+        public static GetNativeLibraryName GetNativeLibraryName;
+
         /// <summary>
         ///     Attempts to load a native library using platform nammig convention.
         /// </summary>
@@ -21,29 +63,11 @@ namespace FFmpeg.AutoGen.Native
         ///     but not until an attempt is made to actually use the function (rather than load it). This matches how PInvokes
         ///     behave.
         /// </remarks>
-        public static IntPtr LoadNativeLibraryUsingPlatformNamingConvention(string path, string libraryName, int version)
+        public static IntPtr LoadNativeLibrary(string path, string libraryName, int version)
         {
-#if NET45
-            var fullName = Path.Combine(path, $"{libraryName}-{version}.dll");
+            var nativeLibraryName = GetNativeLibraryName(libraryName, version);
+            var fullName = Path.Combine(path, nativeLibraryName);
             return LoadNativeLibrary(fullName);
-#else
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                var fullName = Path.Combine(path, $"{libraryName}-{version}.dll");
-                return LoadNativeLibrary(fullName);
-            }
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                var fullName = Path.Combine(path, $"lib{libraryName}.so.{version}");
-                return LoadNativeLibrary(fullName);
-            }
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                var fullName = Path.Combine(path, $"lib{libraryName}.{version}.dylib");
-                return LoadNativeLibrary(fullName);
-            }
-            throw new PlatformNotSupportedException();
-#endif
         }
 
         /// <summary>
@@ -61,14 +85,19 @@ namespace FFmpeg.AutoGen.Native
         /// </remarks>
         public static IntPtr LoadNativeLibrary(string libraryName)
         {
-#if NET45
-            return WindowsNativeMethods.LoadLibrary(libraryName);
-#else
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return WindowsNativeMethods.LoadLibrary(libraryName);
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return LinuxNativeMethods.dlopen(libraryName, LinuxNativeMethods.RTLD_NOW);
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) return MacNativeMethods.dlopen(libraryName, MacNativeMethods.RTLD_NOW);
-            throw new PlatformNotSupportedException();
-#endif
+            switch (GetPlatformId())
+            {
+                case PlatformID.MacOSX:
+                    return MacNativeMethods.dlopen(libraryName, MacNativeMethods.RTLD_NOW);
+                case PlatformID.Unix:
+                    return LinuxNativeMethods.dlopen(libraryName, LinuxNativeMethods.RTLD_NOW);
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                    return WindowsNativeMethods.LoadLibrary(libraryName);
+                default:
+                    throw new PlatformNotSupportedException();
+            }
         }
     }
 }
