f2c214194cd1cbf625da960916d86dde4f8df0ef
hanabi1224 <harlowmoo@gmail.com>
2021-03-12 19:49:04 +0800

    fix 1 memleak in example

--- FFmpeg.AutoGen.Example/FFmpegBinariesHelper.cs
+++ FFmpeg.AutoGen.Example/FFmpegBinariesHelper.cs
@@ -7,19 +7,30 @@ namespace FFmpeg.AutoGen.Example
     {
         internal static void RegisterFFmpegBinaries()
         {
-            var current = Environment.CurrentDirectory;
-            var probe = Path.Combine("FFmpeg", "bin", Environment.Is64BitProcess ? "x64" : "x86");
-            while (current != null)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                var ffmpegBinaryPath = Path.Combine(current, probe);
-                if (Directory.Exists(ffmpegBinaryPath))
+                var current = Environment.CurrentDirectory;
+                var probe = Path.Combine("FFmpeg", "bin", Environment.Is64BitProcess ? "x64" : "x86");
+                while (current != null)
                 {
-                    Console.WriteLine($"FFmpeg binaries found in: {ffmpegBinaryPath}");
-                    ffmpeg.RootPath = ffmpegBinaryPath;
-                    return;
-                }
+                    var ffmpegBinaryPath = Path.Combine(current, probe);
+                    if (Directory.Exists(ffmpegBinaryPath))
+                    {
+                        Console.WriteLine($"FFmpeg binaries found in: {ffmpegBinaryPath}");
+                        ffmpeg.RootPath = ffmpegBinaryPath;
+                        return;
+                    }
 
-                current = Directory.GetParent(current)?.FullName;
+                    current = Directory.GetParent(current)?.FullName;
+                }
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                ffmpeg.RootPath = "/lib/x86_64-linux-gnu/";
+            }
+            else
+            {
+                // TODO
             }
         }
     }
