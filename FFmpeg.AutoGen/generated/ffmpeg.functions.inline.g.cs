4da56184d7f9ddf1be30430125bb8445c57f1029
Ruslan Balanukhin <ruslan@rationale.one>
2023-11-14 19:53:13 +0100

    Fix new inline function

--- FFmpeg.AutoGen/generated/ffmpeg.functions.inline.g.cs
+++ FFmpeg.AutoGen/generated/ffmpeg.functions.inline.g.cs
@@ -187,9 +187,9 @@ public static unsafe partial class ffmpeg
     
     /// <summary>Wrapper around av_image_copy() to workaround the limitation that the conversion from uint8_t * const * to const uint8_t * const * is not performed automatically in C.</summary>
     public static void av_image_copy2(ref byte_ptrArray4 @dst_data, in int_array4 @dst_linesizes, ref byte_ptrArray4 @src_data, in int_array4 @src_linesizes, AVPixelFormat @pix_fmt, int @width, int @height)
-{
-    av_image_copy(dst_data, dst_linesizes, (const uint8_t *const *)src_data, src_linesizes, pix_fmt, width, height);
-}
+    {
+        av_image_copy(ref dst_data, dst_linesizes, src_data, src_linesizes, pix_fmt, width, height);
+    }
     // original body hash: RdaM2zKuF7t0cNJItdh1SZPg9WiOfPpTwty5cHDiZ2A=
     
     /// <summary>Reinterpret a 64-bit integer as a double.</summary>
